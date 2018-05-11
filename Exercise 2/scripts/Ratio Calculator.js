1ecf0674efe8fffdb8e7a790cf3be8e684b27509
CarrieAnnCarlton <carrieanncarlton@gmail.com>
2018-05-11 14:36:39 -0500

    Initial file population

---
+++ Exercise 2/scripts/Ratio Calculator.js
@@ -0,0 +1,15 @@
+/*
+This function helps "Exercise 2/index.html" to calculate a ratio.
+@author:     Carrie Carlton
+@modified:   11 May 18
+*/
+
+"use strict";
+
+function computeRatio() {
+	var userWidth = document.getElementById("userWidth");
+	var userHeight = document.getElementById("userHeight");	
+	var desiredWidth = document.getElementById("desiredWidth");	//check something entered/not entered
+	var desiredHeight = document.getElementById("desiredHeight"); //check something entered/not entered
+
+}//function
